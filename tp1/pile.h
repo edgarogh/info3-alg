c4c768e9baa1522e9aeb25fa2a00d96321204622
Edgar Onghena <dev@edgar.bzh>
2022-02-02 08:31:37 +0100

    Add header guards

--- tp1/pile.h
+++ tp1/pile.h
@@ -1,6 +1,7 @@
+#ifndef _PILE_H_
+#define _PILE_H_
 
 #define MAX_PILE_SIZE       32
-
 
 typedef struct {
   int sommet ;
@@ -20,3 +21,5 @@ pnoeud_t depiler (ppile_t p)  ;
 int empiler (ppile_t p, pnoeud_t pn) ;
 
 void imprimer_pile(ppile_t p);
+
+#endif
